4b1bba8ab60f1f99c5413058717a701affbb6bbe
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2021-09-05 13:25:52 +0300

    update task #8

--- js/task-08.js
+++ js/task-08.js
@@ -13,21 +13,20 @@ function getAmount() {
   createBoxes(amount);
 }
 
-function createBoxes(amount) {
-  let widthBox = 30;
-  let heightBox = 30;
+let sizeBox = 30;
 
+function createBoxes(amount) {
   const arrayBox = [];
+
   for (let i = 0; i < amount; i += 1) {
     const boxesContainer = document.createElement('div');
 
-    boxesContainer.style.width = `${widthBox}px`;
-    boxesContainer.style.height = `${heightBox}px`;
+    boxesContainer.style.width = `${sizeBox}px`;
+    boxesContainer.style.height = `${sizeBox}px`;
     boxesContainer.style.backgroundColor = `rgb(${getRGB()}, ${getRGB()},${getRGB()})`;
 
     arrayBox.push(boxesContainer);
-    widthBox += 10;
-    heightBox += 10;
+    sizeBox += 10;
   }
 
   refs.boxesEl.append(...arrayBox);
@@ -36,6 +35,7 @@ function createBoxes(amount) {
 function destroyBoxes() {
   refs.boxesEl.innerHTML = '';
   refs.inputControls.value = '';
+  sizeBox = 30;
 }
 
 function getRGB() {
